f4fbec7326cc3746890c3f43f1fde4424c3722bc
Programatik <programatik29@gmail.com>
2021-02-07 18:43:09 +0300

    Switched high level "Client" api to low level "conn" api

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use hyper::{Request, Body};
+use hyper::{Request, Body, Uri};
 
 const GIGABYTE: f64 = (1024 * 1024 * 1024) as f64;
 const MEGABYTE: f64 = (1024 * 1024) as f64;
@@ -13,6 +13,24 @@ pub fn get_request(host: &str) -> Request<Body> {
         .method("GET")
         .body(Body::from(""))
         .expect("Failed to build request")
+}
+
+/// Constructs a new Request of a given host.
+pub fn get_request_new(uri: &Uri) -> Request<Body> {
+    let host = host_header(uri);
+
+    Request::builder()
+        .uri(uri)
+        .header("Host", host)
+        .method("GET")
+        .body(Body::from(""))
+        .expect("Failed to build request")
+}
+
+fn host_header(uri: &Uri) -> String {
+    let invalid_uri = "Invalid URI";
+
+    format!("{}:{}", uri.host().expect(invalid_uri), uri.port_u16().expect(invalid_uri))
 }
 
 /// Dirt simple div mod function.
